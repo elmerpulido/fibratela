874d6b349aac896bad296e923a0d29545dbd9d04
pclinux <pclinux@local.co>
2022-11-08 16:00:02 -0500

    agregando Categorias

--- web/views/modules/header.php
+++ web/views/modules/header.php
@@ -5,58 +5,25 @@
 
     <div class="row header__elment">
 
+
+        <!-- logo -->
+
         <div class="col-sm-6 col-md-12 col-xl-2 header__logo">
             <a href="#">Fibratela</a>            
         </div>
 
+
+        <!-- categorias -->
+
         <div class="col-sm-6 col-md-3 col-xl-1 header__categorias">
 
-                <button class="header_btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">Categorias
-                <i class="fa-solid fa-bars"></i></button>
-
-                <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop">
-                <div class="offcanvas-header">   
-                    
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close">
-                    <i class="fa-solid fa-x"></i>
-                    </button>
-                </div>
-                <div class="offcanvas-body">
-
-                    <div class="container info__categorias">
-                        <div class="row">
-                            
-                            <div class="col-xl-3">
-                                <div class="col-6">
-                                    <div><h5>Dril</h5>
-                                    <ul>
-                                        <li>tela1</li>
-                                        <li>tela1</li>
-                                        <li>tela1</li>
-                                    </ul>
-                                </div>
-                                    <div><h5>Imagen</h5></div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3">
-                                <div class="col-6">
-                                    <div><h5>Dril</h5></div>
-                                    <div><h5>Imagen</h5></div>
-                                </div>
-                            </div>
-                            <div class="col-xl-3">
-                                <div class="col-6">
-                                    <div><h5>Dril</h5></div>
-                                    <div><h5>Imagen</h5></div>
-                                </div>
-                            </div>
-                            
-                        </div>
-                    </div>     
-                </div>
-                </div>
+        <a href="#"> Categorias
+            <i class="fa-thin fa-bars"></i>
+        </a>
 
         </div>
+
+        <!-- Redes Sociales -->
 
         <div class="col-sm-12 col-md-4 col-xl-3 header_RedSocial">
             <a href="#">
@@ -70,6 +37,8 @@
             </a>
         </div>
 
+        <!-- Carro compras -->
+
         <div class="col-sm-12 col-md-2 col-xl-1 header_shopCar">
 
             <a href="#">
@@ -79,6 +48,8 @@
             
         </div>
 
+       <!-- Login -->
+
         <div class="col-sm-12 col-md-2 col-xl-2 header_login">
 
             <a href="#"> 
@@ -86,6 +57,8 @@
                 <span>Login</span>
             </a>            
         </div>
+
+        <!-- Pefil -->
 
         <div class="col-sm-12 col-md-2 col-xl-3 header_perfil">
 
