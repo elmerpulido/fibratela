979c94d52abf9a1b8a74ac9928484e8777e9e7e4
pclinux <pclinux@local.co>
2022-11-10 17:02:43 -0500

    creando menu principal con flexbox

---
+++ web/views/modules/header.php
@@ -0,0 +1,63 @@
+
+<header class="header">
+
+    <div class="header-top">
+
+        <div class="header-logo">
+
+        <a href="#" class="header-a">
+            <h1 class="header-h1">Fibratela</h1>
+        </a>
+        </div>
+
+        <div class="header-categorias">
+
+            <a href="#" class="header-a">
+                <h3 class="header-h3">Categorias</h3>
+                <i class="fa-solid fa-bars"></i>
+            </a>
+
+        </div>
+
+        <div class="header-redsocial">
+
+            <ul class="header-ul">
+
+                <li class="header-li">
+                    <a href="" class="header-a">
+                    <i class="fa-brands fa-facebook-f"></i>
+                    </a>
+                </li>
+                <li class="header-li">
+                    <a href="" class="header-a">
+                    <i class="fa-brands fa-instagram"></i>
+                    </a>
+                </li>
+                <li class="header-li">
+                    <a href="" class="header-a">
+                    <i class="fa-brands fa-whatsapp"></i>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+
+        <div class="header-login">
+            <ul class="header-ul">
+                <li class="header-li">
+                    <a href="#" class="header-a">
+                    <i class="fa-regular fa-user"></i>
+                    <h3 class="header-h3">Ingresar</h3>
+                    </a>
+                </li>                
+                <li class="header-li">
+                    <a href="#" class="header-a">
+                    <i class="fa-regular fa-address-card"></i>
+                    <h3 class="header-h3">Registro</h3>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+</header>
